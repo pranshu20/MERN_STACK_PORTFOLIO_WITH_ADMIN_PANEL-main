cb85ab5fca4248acade49cda9a6d8e79ae5e1a14
Pranshu Goyal <pranshugoyal20022003@gmail.com>
2024-06-17 16:44:49 +0530

    Initial commit

---
+++ portfolio/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
